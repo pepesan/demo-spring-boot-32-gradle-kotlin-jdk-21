8b1cc73b8039532f31bfea205c0d192baaf57876
David Vaquero <pepesan@gmail.com>
2023-12-18 17:06:41 +0100

    corregido el controller Advice

--- src/main/kotlin/com/example/demogradlekotlin/advices/ControllerExceptionHandler.kt
+++ src/main/kotlin/com/example/demogradlekotlin/advices/ControllerExceptionHandler.kt
@@ -12,16 +12,52 @@ import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.context.request.WebRequest
 import java.util.*
-
+//
+//@ControllerAdvice
+//@Slf4j
+//class ControllerExceptionHandler {
+//    @ExceptionHandler(value = [ResourceNotFoundException::class])
+//    fun resourceNotFoundException(ex: ResourceNotFoundException, request: WebRequest?): ResponseEntity<ErrorMessage> {
+//        val message: ErrorMessage = ErrorMessage(
+//            404,
+//            Date(),
+//            ex.message,
+//            "Error capturado por ResourceNotFoundException"
+//        )
+//        return ResponseEntity<ErrorMessage>(
+//            message,
+//            HttpStatus.NOT_FOUND
+//        )
+//    }
+//
+//    @ExceptionHandler(MethodArgumentNotValidException::class)
+//    fun handleValidationException(ex: MethodArgumentNotValidException): ResponseEntity<ErrorMessage> {
+//        // Obtén los mensajes de error de la excepción
+//        val errorMessage = ex.bindingResult.allErrors[0].defaultMessage
+//
+//        // Puedes personalizar la respuesta según tus necesidades
+//        val apiError: ErrorMessage = ErrorMessage(
+//            400,
+//            Date(),
+//            errorMessage,
+//            "Error capturado por MethodArgumentNotValidException"
+//        )
+//        val headers = HttpHeaders()
+//        headers.contentType = MediaType.APPLICATION_JSON
+//        // Devuelve la respuesta con el código de estado adecuado
+//        return ResponseEntity<ErrorMessage>(apiError, headers, HttpStatus.BAD_REQUEST)
+//    }
+//}
 @ControllerAdvice
-@Slf4j
-class ControllerExceptionHandler {
-    @ExceptionHandler(value = [ResourceNotFoundException::class])
-    fun resourceNotFoundException(ex: ResourceNotFoundException, request: WebRequest?): ResponseEntity<ErrorMessage> {
+class ResourceNotFoundExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException::class)
+
+    fun handleValidationException(e: ResourceNotFoundException): ResponseEntity<ErrorMessage> {
         val message: ErrorMessage = ErrorMessage(
             404,
             Date(),
-            ex.message,
+            e.message,
             "Error capturado por ResourceNotFoundException"
         )
         return ResponseEntity<ErrorMessage>(
@@ -29,9 +65,9 @@ class ControllerExceptionHandler {
             HttpStatus.NOT_FOUND
         )
     }
-
     @ExceptionHandler(MethodArgumentNotValidException::class)
-    fun handleValidationException(ex: MethodArgumentNotValidException): ResponseEntity<ErrorMessage> {
+
+    fun handleResourceNotFound(ex: MethodArgumentNotValidException): ResponseEntity<ErrorMessage> {
         // Obtén los mensajes de error de la excepción
         val errorMessage = ex.bindingResult.allErrors[0].defaultMessage
 
